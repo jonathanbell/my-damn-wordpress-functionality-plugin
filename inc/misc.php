a54d6222babb8a3157716a8d587909482821770f
Jonathan Bell <jonathanbell.ca@gmail.com>
2016-03-08 12:32:15 -0800

    adding jsfiddle embeds hopefully

--- inc/misc.php
+++ inc/misc.php
@@ -8,12 +8,13 @@ function wp_embed_handler_jsfiddle($matches, $attr, $url, $rawattr) {
     esc_attr($matches[2])
   );
 
-  return apply_filters('wp_embed_handler_jsfiddle', $embed, $matches, $attr, $url, $rawattr);
+  // return apply_filters('wp_embed_handler_jsfiddle', $embed, $matches, $attr, $url, $rawattr);
+  return '<iframe width="100%" height="300" src="//jsfiddle.net/jonnymilano/pd5boL42/embedded/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
 
 } // wp_embed_handler_jsfiddle
 
 function mdwpbp_misc() {
   wp_oembed_add_provider('http://codepen.io/*/pen/*', 'http://codepen.io/api/oembed', false);
   wp_oembed_add_provider('#http://(www\.)?vimeo\.com/.*#i', 'http://vimeo.com/api/oembed.{format}', true);
-  wp_embed_register_handler('jsfiddle', '#http(?:s)://jsfiddle.net/(?:[a-zA-Z]+)/(?:^[a-zA-Z0-9_.-]*$)/*', 'wp_embed_handler_jsfiddle');
+  wp_embed_register_handler('jsfiddle', '#https?://jsfiddle.net/.*#i', 'wp_embed_handler_jsfiddle');
 }
