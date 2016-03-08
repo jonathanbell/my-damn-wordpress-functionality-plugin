70794e26b814b00dbbab65dd1c81152297aba62f
Jonathan Bell <jonathanbell.ca@gmail.com>
2016-03-08 12:02:38 -0800

    adding jsfiddle embeds hopefully

--- inc/misc.php
+++ inc/misc.php
@@ -1,4 +1,19 @@
 <?php
 
-// add CodePen to oEmbed provider whitelist
-wp_oembed_add_provider( 'http://codepen.io/*/pen/*', 'http://codepen.io/api/oembed' );
+function wp_embed_handler_jsfiddle($matches, $attr, $url, $rawattr) {
+
+  $embed = sprintf(
+    '<iframe width="100%" height="300" src="//jsfiddle.net/%1$s/%2$s/embedded/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
+    esc_attr($matches[1]),
+    esc_attr($matches[2])
+  );
+
+  return apply_filters('embed_jsfiddle', $embed, $matches, $attr, $url, $rawattr);
+
+} // wp_embed_handler_jsfiddle
+
+function mdwpbp_misc() {
+  wp_oembed_add_provider('http://codepen.io/*/pen/*', 'http://codepen.io/api/oembed', false);
+  wp_oembed_add_provider('#http://(www\.)?vimeo\.com/.*#i', 'http://vimeo.com/api/oembed.{format}', true);
+  wp_embed_register_handler('jsfiddle', '#http(?:s)://jsfiddle.net/(?:[a-zA-Z]+)/(?:^[a-zA-Z0-9_.-]*$)/*', 'wp_embed_handler_jsfiddle');
+}
--- mdwpfp.php
+++ mdwpfp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       My Damn Wordpress Functionality Plugin
  * Plugin URI:        https://github.com/jonathanbell/my-damn-wordpress-functionality-plugin
- * Description:       General functionality for my (and your?) Wordpress sites.
+ * Description:       General functionality for my (and your?) Wordpress site(s).
  * Version:           0.0.1-alpha
  * Author:            Jonathan Bell
  * Author URI:        http://30.jonathanbell.ca
@@ -12,7 +12,8 @@
  * Domain Path:       /lang
  */
 
-reqUire_once('inc/clean-up-head.php'); // stuff that cleans up the <head>
+require_once('inc/clean-up-head.php'); // stuff that cleans up the <head>
 require_once('inc/misc.php'); // other stuff
 
 add_action('after_setup_theme', 'mdwpfp_init');
+add_action('init', 'mdwpbp_misc');
